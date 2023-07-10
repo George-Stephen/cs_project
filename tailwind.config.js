9dfa7ff113908b4ee47b294145c94d8c01e88a21
George Stephen <georgesteve561@gmail.com>
2023-07-10 18:49:12 +0300

    working on the UI

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,10 @@ export default {
         './vendor/laravel/jetstream/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+        "./node_modules/tw-elements/dist/js/**/*.js"
     ],
 
     theme: {
@@ -19,5 +23,5 @@ export default {
         },
     },
 
-    plugins: [forms, typography],
+    plugins: [forms, typography,require("tw-elements/dist/plugin.cjs")],
 };
